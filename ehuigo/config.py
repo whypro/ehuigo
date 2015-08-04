0a7d6bf72cb9d0280e98b8fda725cab06ec3be34
whypro <whypro@live.cn>
2015-08-04 16:59:34 +0800

    Fix bugs of config.py

--- ehuigo/config.py
+++ ehuigo/config.py
@@ -15,23 +15,20 @@ class Config(object):
     DB_PASSWORD = 'whypro'
     DB_PORT = 3306
 
+    # FLASK-SQLALCHEMY
+    SQLALCHEMY_DATABASE_URI = 'mysql://{username}:{password}@{host}:{port}/{database}?charset=utf8'.format(
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_DATABASE
+    )
+
     # Flask-SQLAlchemy Debugging Option
     # SQLALCHEMY_ECHO = True
 
     HOT_PRODUCT_NUM = 4
     HOT_MANUFACTURER_NUM = 6
-
-    @property
-    def SQLALCHEMY_DATABASE_URI(self):
-        # FLASK-SQLALCHEMY
-        uri = 'mysql://{username}:{password}@{host}:{port}/{database}?charset=utf8'.format(
-            username=self.DB_USERNAME,
-            password=self.DB_PASSWORD,
-            host=self.DB_HOST,
-            port=self.DB_PORT,
-            database=self.DB_DATABASE
-        )
-        return uri
 
 
 class BAEConfig(Config):
@@ -42,4 +39,15 @@ class BAEConfig(Config):
     DB_USERNAME = 'AF28c466e7dd74686195abc876d4849b'
     DB_PASSWORD = '2b2f8ac70616dbea89fc2ac2312de1a9'
     DB_PORT = 4050
+
+    # FLASK-SQLALCHEMY
+    SQLALCHEMY_DATABASE_URI = 'mysql://{username}:{password}@{host}:{port}/{database}?charset=utf8'.format(
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        host=DB_HOST,
+        port=DB_PORT,
+        database=DB_DATABASE
+    )
+
+    DEBUG = True
 
