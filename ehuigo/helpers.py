946550b8e244695de30b701fc9f63fe9a450fbdc
whypro <whypro@live.cn>
2015-12-18 21:51:16 +0800

    将发送邮件改为单线程。

--- ehuigo/helpers.py
+++ ehuigo/helpers.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from threading import Thread
-from smtplib import SMTPServerDisconnected
-from ssl import SSLError
-import time
 
 from flask import current_app, render_template, request
 from flask.ext.mail import Message
@@ -20,7 +17,11 @@ def create_uploader():
     return uploader
 
 
+# 有 BUG
 def _async_send_email(app, msg, retry=3):
+    from smtplib import SMTPServerDisconnected
+    from ssl import SSLError
+    import time
     with app.app_context():
         for i in range(retry):
             try:
@@ -37,10 +38,10 @@ def _async_send_email(app, msg, retry=3):
 def send_email(to, subject, template, **kwargs):
     msg = Message(subject=subject, sender=current_app.config['EHUIGO_MAIL_SENDER'], recipients=[to])
     msg.html = render_template(template, **kwargs)
-    thread = Thread(target=_async_send_email, args=[current_app._get_current_object(), msg])
-    thread.start()
-    return thread
-    # mail.send(msg)
+    # thread = Thread(target=_async_send_email, args=[current_app._get_current_object(), msg])
+    # thread.start()
+    # return thread
+    mail.send(msg)
 
 
 def get_client_ip():
